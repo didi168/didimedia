fde17aea0e0853a958db0a3332b84b9a2c738986
Divine Chukwudumebi Okorie <96950772+didi168@users.noreply.github.com>
2023-01-22 17:28:13 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction(){
+ document.getElementsByTagName("nav")[0].style.display = "block"
+  
+}
+function meRe(){
+  document.getElementsByTagName("nav")[0].style.display = "none"
+}
